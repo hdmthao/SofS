157b227fb7f4951ffc216b98919ee35a822f0d71
hdmthao <minhthaohaduc@gmail.com>
2021-01-11 12:06:06 +0700

    add sequelize production config

---
+++ backend/config/env/config.production.js
@@ -0,0 +1,12 @@
+{
+  "databaseConfig": {
+    "username": "aka",
+    "password": "18apcs",
+    "database": "sofs_db",
+    "host": "127.0.0.1",
+    "port": 5432,
+    "dialect": "postgres",
+    "logging": false,
+    "use_env_variable": "DATABASE_URL"
+  }
+}
